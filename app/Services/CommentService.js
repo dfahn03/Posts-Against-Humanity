92d470b735ad3fe6f2aead0d73d55a3d204ca90e
Dallin Fahnestock <dfahn03@gmail.com>
2019-05-31 14:32:23 -0600

    BE: W/o esm

--- app/Services/CommentService.js
+++ app/Services/CommentService.js
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose'
 
 //PRIVATE
-// let ObjectId = mongoose.Schema.Types.ObjectId
+
+let ObjectId = mongoose.Schema.Types.ObjectId
 
 let _schema = new mongoose.Schema({
-  title: { type: String, required: true },
-  category: { type: String, enum: ['Conspiracy', 'Wrong-Answer', 'Mad-Libs'], required: true },
   body: { type: String, required: true },
-  votes: { type: Number, default: 0 },
-  userId: { type: String, required: true }
+  values: { type: Number, default: 0 },
+  questionId: { type: ObjectId, required: true },
+  userId: { type: ObjectId, required: true }
 }, { timestamps: true })
 
 //PUBLIC
--- app/main.js
+++ app/main.js
@@ -8,11 +8,16 @@ server.use(bp.urlencoded({ extended: true }))
 server.use(bp.json())
 
 //register routes
-// import UserController from './controllers/UserController'
+import UserController from './controllers/UserController'
+import QuestionController from './controllers/QuestionController'
+import CommentController from './controllers/CommentController'
+
+server.use('/api/users', new UserController().router)
+server.use('/api/questions', new QuestionController().router)
+server.use('/api/comments', new CommentController().router)
 
 
 
-// server.use('/api/users', new UserController().router)
 
 
 
