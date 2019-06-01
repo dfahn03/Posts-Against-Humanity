345dca3c54a5e2dfd8d01a01480e1aa676801356
Dallin Fahnestock <dfahn03@gmail.com>
2019-06-01 00:02:37 -0600

    update time on comment.js and CommentServ back end

--- client/app/models/Comment.js
+++ client/app/models/Comment.js
@@ -5,6 +5,7 @@ export default class Comment {
     this.values = data.values
     this.questionId = data.questionId || data.questionId._id
     this.userId = data.userId || data.userId._id
+    this.timestamp = data.timestamp || moment(data.createdAt.split('T')[0], "YYYYMMDD").startOf('day').fromNow()
   }
 
 
