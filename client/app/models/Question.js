3d75784be3aabc115953d777479b0fe3534315a3
Dallin Fahnestock <dfahn03@gmail.com>
2019-05-31 22:53:20 -0600

    gonna get buttons connected

--- client/app/models/Question.js
+++ client/app/models/Question.js
@@ -18,7 +18,7 @@ export default class Question {
     return `<div class="individual-post d-flex not-the-d-fleex card mb-3">
       <h3 class="mix-text-difference card-header">${this.title}</h3>
       <div class="card-body">
-        <h5 class="mix-text-difference card-title">${this.byLine}</h5>
+        <h5 class="mix-text-difference card-title">@${this.byLine}</h5>
       </div>
       <img class="post-img" style="width: 50%;" src="/assets/hitler-in-old-age-in-south-america.png" alt="Post image">
       <div class="card-body">
@@ -29,7 +29,7 @@ export default class Question {
       </ul>
       <div class="card-body like-body d-flex">
       <a href="#" class="card-link"><img class="vote-icon" src="assets/thumbs_up_emoji_by_google_pFE_icon.ico"></img></a>
-      <span>Public opinion:${this.votes} Likes</span>
+      <span>Public opinion: ${this.votes} Likes</span>
       <a href="#" class="card-link"><img class="vote-icon" src="assets/1f595_nkA_icon.ico"></img></a>
       </div>
       <div class="mix-text-difference card-footer text-muted">
